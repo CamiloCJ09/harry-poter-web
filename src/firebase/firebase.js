10f284c2dfbf593b9d748742b266590e9a443c2b
Johan Stiven Ricardo <johan.ricardo1104@gmail.com>
2023-09-24 16:40:17 -0500

    feat: added firebase credentials to .env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoT5nfKbgA4QBk31VkUhd10jrbcvcDp-Q",
-  authDomain: "harry-poter-web.firebaseapp.com",
-  projectId: "harry-poter-web",
-  storageBucket: "harry-poter-web.appspot.com",
-  messagingSenderId: "209536513845",
-  appId: "1:209536513845:web:cb9635c4330c897be27e7c",
-  measurementId: "G-J060HXZYRM",
+  apiKey: import.meta.env.VITE_REACT_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_AUTH_DOMAIN,
+  rojectId: import.meta.env.VITE_REACT_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
